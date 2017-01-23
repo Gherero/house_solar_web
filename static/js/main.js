3bb53cf627a126829839607bdc980cfc7e13e3eb
gherero <katerina18>
2017-01-23 11:40:00 +0200

    1

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,9 @@
-/**
- * Created by kiro on 13.01.17.
- */
-
 $(document).ready(function() {
-	$(chart_id).highcharts({
-		chart: chart,
-		title: title,
-		xAxis: xAxis,
-		yAxis: yAxis,
-		series: series
-	});
+    $(chart_id).highcharts({
+        chart: chart,
+        title: title,
+        xAxis: xAxis,
+        yAxis: yAxis,
+        series: series
+    });
 });
